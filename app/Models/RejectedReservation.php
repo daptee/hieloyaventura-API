64b754488f16b3c18ff674caca162760a1260cec
Jean Piere <javierjeanpieres@gmail.com>
2022-09-16 12:40:30 -0300

    add - update

--- app/Models/RejectedReservation.php
+++ app/Models/RejectedReservation.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class RejectedReservation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_reservation_id',
+        'payment_id',
+        'data'
+    ];
 }
