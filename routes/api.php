9a2adb6b0324109fa6afeda42caba3b4b407feee
Jean Piere <javierjeanpieres@gmail.com>
2022-09-29 13:50:24 -0300

    add - nueva ruta para editar datos del user

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::controller(AuthController::class)->group(function () {
 });
 Route::controller(UserController::class)->group(function () {
     Route::post('register', 'register');
+    Route::put('user_edit', 'update')->middleware(['jwt.verify']);
 });
 Route::get('faqs', [FaqController::class, 'index']);
 Route::prefix('excurtions')->controller(ExcurtionController::class)->group(function () {
