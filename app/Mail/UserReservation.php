a34ac35c5a35cd0f11ed4356f015b3ab3f9561e0
Jean Piere <javierjeanpieres@gmail.com>
2022-09-30 18:18:55 -0300

    add - imgenes y mail para pago exitoso

---
+++ app/Mail/UserReservation.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class UserReservation extends Mailable
+{
+    // use Queueable, SerializesModels;
+    public $subjet = "Reserva creada exitosamente";
+
+    public $email;
+    public $pathPdf;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($email, $pathPdf)
+    {
+        $this->email   = $email;
+        $this->pathPdf = $pathPdf;
+
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('info@hieloyaventuras.com.ar', 'Hielo y aventuras')
+                    ->attach($this->pathPdf)
+                    ->replyTo('info@hieloyaventuras.com.ar')
+                    ->subject($this->subject)
+                    ->view('emails.user-reservation')
+                    ->with(["msg" => "El pago fue exitoso."])
+                    ;
+    }
+}
