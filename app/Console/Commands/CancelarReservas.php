b5b9aedb9f60f3184e2a636c442e41d177ba779b
EnzoAmarilla <enzo100amarilla@gmail.com>
2023-01-11 17:27:54 -0300

    CURL cancelar reservar

--- app/Console/Commands/CancelarReservas.php
+++ app/Console/Commands/CancelarReservas.php
@@ -5,6 +5,9 @@ namespace App\Console\Commands;
 use App\Models\ReservationStatus;
 use App\Models\UserReservation;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+// use Illuminate\Support\Facades\Http;
 
 class CancelarReservas extends Command
 {
@@ -33,10 +36,16 @@ class CancelarReservas extends Command
                                     ->where('created_at', '<', now()->modify('-30 minute')->format('Y-m-d H:i:s'))
                                     ->get();
 
-        foreach($reservations as $reservation){
-            $reservation->reservation_status_id = ReservationStatus::AUTOMATIC_CANCELED;
-            $reservation->save();
-            echo $reservation->id . ' - ';
+        if(count($reservations)){
+            foreach($reservations as $reservation){
+                $reservation->reservation_status_id = ReservationStatus::AUTOMATIC_CANCELED;
+                $reservation->save();
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, "http://apihya.hieloyaventura.com/apihya/CancelaReserva?RSV=$reservation->reservation_number");
+                curl_setopt($ch, CURLOPT_POST, 1);
+                $data = curl_exec($ch);
+                curl_close($ch);
+            }
         }
     }
 }
