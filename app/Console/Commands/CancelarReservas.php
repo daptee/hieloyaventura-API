b01b1f46e3d797b5bb6816b884b14e527acf4ea4
EnzoAmarilla <enzo100amarilla@gmail.com>
2023-04-19 18:50:34 -0300

    proceso cancelar reserva script completo con logs

--- app/Console/Commands/CancelarReservas.php
+++ app/Console/Commands/CancelarReservas.php
@@ -35,19 +35,20 @@ class CancelarReservas extends Command
     {
         $reservations = UserReservation::whereIn('reservation_status_id', [ReservationStatus::REJECTED, ReservationStatus::STARTED])
                                     ->where('created_at', '<', now()->modify('-30 minute')->format('Y-m-d H:i:s'))
+                                    ->where('reservation_number', '!=', 0)
                                     ->get();
         
         // Log::debug($reservations);
-        // Log::debug("Cantidad de reservas que trae la query: " . count($reservations));
+        Log::debug("Cantidad de reservas que trae la query: " . count($reservations));
+        $url = config('app.api_hya')."/CancelaReservaM2";
+        Log::debug("Url api: " . $url);
 
         if(count($reservations) > 0){
             foreach($reservations as $reservation){
                 
-                Log::debug("Numero de reserva: $reservation->reservation_number");
-                
                 $curl = curl_init();
                 $fields = json_encode( array("RSV" => $reservation->reservation_number) );
-                curl_setopt($curl, CURLOPT_URL, env("API_HYA")."/CancelaReservaM2");
+                curl_setopt($curl, CURLOPT_URL, $url);
                 curl_setopt($curl, CURLOPT_POST, true);
                 curl_setopt($curl, CURLOPT_POSTFIELDS, $fields);
                 curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
@@ -56,12 +57,15 @@ class CancelarReservas extends Command
                 $resp = curl_exec($curl);
                 curl_close($curl);
 
-                // Log::debug("Response: $resp");
+                Log::debug("Respuesta API: $resp");
 
-                // !is_null($resp)
+                $resultado = isset(json_decode($resp)->RESULT) ? json_decode($resp)->RESULT : "Sin resultado";
+                $mensaje = isset(json_decode($resp)->ERROR_MSG) ? json_decode($resp)->ERROR_MSG : "Sin mensaje";
+
+                Log::debug("Numero de reserva: $reservation->reservation_number , Resultado API: $resultado , MSG: $mensaje");
 
                 if(isset(json_decode($resp)->RESULT)){
-                    if(json_decode($resp)->RESULT == "OK"){
+                    if(json_decode($resp)->RESULT == "OK" || json_decode($resp)->ERROR_MSG == "RSV:$reservation->reservation_number NO ENCONTRADA"){
                         $reservation->reservation_status_id = ReservationStatus::AUTOMATIC_CANCELED;
                         $reservation->save();
 
