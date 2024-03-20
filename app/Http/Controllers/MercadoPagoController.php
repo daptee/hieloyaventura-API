2c28a8a7454073d1d874c2fe36628d713f60536c
EnzoAmarilla <enzo100amarilla@gmail.com>
2024-03-20 15:40:21 -0300

    notificacion webhook MP envio de email

--- app/Http/Controllers/MercadoPagoController.php
+++ app/Http/Controllers/MercadoPagoController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\MercadoPagoNotification;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use MercadoPago;
 use stdClass;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Mail;
 
 class MercadoPagoController extends Controller
 {
@@ -79,32 +81,49 @@ class MercadoPagoController extends Controller
 
     public function notificationWebHook(Request $request)
     {
-        Log::channel("notificationmp")->info($request);
+        Log::channel("notificationmp")->info("request data: " . json_encode($request->all()));
         $payment = null;
         try {
-            // $token = config('services.mercadopago.webhook.token');
             $token = config('services.mercadopago.dev.token');
-            Log::channel("notificationmp")->info("token: $token");
-            MercadoPago\SDK::setAccessToken($token);
-            $data = $request;
-            Log::channel("notificationmp")->info("data: $data");
-            $type = $data->type ?? null;
-            Log::channel("notificationmp")->info("type: $type");
-            $id = $data->data['id'] ?? $data->data->id;
-            Log::channel("notificationmp")->info("id: $id");
-            $payment = MercadoPago\Payment::find_by_id($id);
-            Log::channel("notificationmp")->info($payment);
 
-            $payment2 = Http::withHeaders([
-                'Authorization' => 'Bearer '.$token,
-                'Content-Type' => 'application/json',
-            ])->get("https://api.mercadopago.com/v1/payments/$id");
+            if($request->type == "payment"){
+                MercadoPago\SDK::setAccessToken($token);
+                $id = $request->data['id'] ?? $request->data->id;
 
-            Log::channel("notificationmp")->info($payment2);
+                $response_payment = Http::withHeaders([
+                    'Authorization' => 'Bearer '.$token,
+                    'Content-Type' => 'application/json',
+                ])->get("https://api.mercadopago.com/v1/payments/$id");
+
+
+                if($response_payment->status() != 200){
+                    Log::channel("notificationmp")->info("error GET Payment: $id");
+                    Log::channel("notificationmp")->info("message error: " . json_encode($response_payment->json()));
+                }else{
+                    $payment = $response_payment->json();
+    
+                    $payment_status = $payment['status'] ?? $payment->status;
+                    $external_reference = $payment['external_reference'] ?? $payment->external_reference;
+    
+                    if($payment_status == "cancelled" || $payment_status == "rejected" || $payment_status == "refunded" || $payment_status == "charged_back"){
+                        try {
+                            Mail::to("ecommerce@hieloyaventura.com")->send(new MercadoPagoNotification($payment_status, $id, $external_reference));                        
+                        } catch (Exception $e) {
+                            Log::channel("notificationmperror")->info("error: " . $e->getMessage() . ", error en envio de mail a ventas@hieloyaventura.com, numero de pago: $id" . ", line: " . $e->getLine());
+                        }
+                    }
+    
+                    Log::channel("notificationmp")->info("response payment json: " . json_encode($response_payment->json()));
+                }
+
+            }
+
         } catch (Exception $e) {
-            Log::channel("notificationmperror")->info('error: ' . $e->getMessage());
+            Log::channel("notificationmperror")->info('error: ' . $e->getMessage() . ', line: ' . $e->getLine());
         }
         
+        Log::channel("notificationmp")->info("<------------------ ------------------>");
+
         return response()->json(["payment" => $payment], 200);
     }
 
