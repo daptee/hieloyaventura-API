51e19cb8444713337decb93e74801751ee8a7b45
EnzoAmarilla <enzo100amarilla@gmail.com>
2024-04-05 15:37:12 -0300

    id payments webhook

--- app/Http/Controllers/MercadoPagoController.php
+++ app/Http/Controllers/MercadoPagoController.php
@@ -74,41 +74,66 @@ class MercadoPagoController extends Controller
     {
         Log::channel("notificationmp")->info("request data: " . json_encode($request->all()));
         $payment = null;
+        $id = null;
+        $payment_status = null;
+        $external_reference = null;
         try {
             $token = config('services.mercadopago.dev.token');
 
             if($request->type == "payment"){
                 MercadoPago\SDK::setAccessToken($token);
-                $id = $request->data['id'] ?? $request->data->id;
 
-                $response_payment = Http::withHeaders([
-                    'Authorization' => 'Bearer '.$token,
-                    'Content-Type' => 'application/json',
-                ])->get("https://api.mercadopago.com/v1/payments/$id");
-
-
-                if($response_payment->status() != 200){
-                    Log::channel("notificationmp")->info("error GET Payment: $id");
-                    Log::channel("notificationmp")->info("message error: " . json_encode($response_payment->json()));
-                }else{
-                    $payment = $response_payment->json();
-    
-                    $payment_status = $payment['status'] ?? $payment->status;
-                    $external_reference = $payment['external_reference'] ?? $payment->external_reference;
-    
-                    if($payment_status == "cancelled" || $payment_status == "rejected" || $payment_status == "refunded" || $payment_status == "charged_back"){
-                        try {
-                            Mail::to("ecommerce@hieloyaventura.com")->send(new MercadoPagoNotification($payment_status, $id, $external_reference));                        
-                        } catch (Exception $e) {
-                            Log::channel("notificationmperror")->info("error: " . $e->getMessage() . ", error en envio de mail a ventas@hieloyaventura.com, numero de pago: $id" . ", line: " . $e->getLine());
-                        }
-                    }
-    
-                    Log::channel("notificationmp")->info("response payment json: " . json_encode($response_payment->json()));
+                if(isset($request['data']['id'])){
+                    $id = $request['data']['id']; 
+                }else if(isset($request->data->id)){
+                    $id = $request->data->id;
                 }
 
-            }
+                // if(isset($id)){
+                //     $response_payment = Http::withHeaders([
+                //         'Authorization' => 'Bearer '.$token,
+                //         'Content-Type' => 'application/json',
+                //     ])->get("https://api.mercadopago.com/v1/payments/$id");
 
+
+                //     if($response_payment->status() != 200){
+                //         Log::channel("notificationmp")->info("error GET Payment: $id");
+                //         Log::channel("notificationmp")->info("message error: " . json_encode($response_payment->json()));
+                //     }else{
+                //         $payment = $response_payment->json();
+        
+                //         if(isset($payment['status'])){
+                //             $payment_status = $payment['status'];
+                //         }else if(isset($payment->status)){
+                //             $payment_status = $payment->status;
+                //         }
+
+                //         if(isset($payment['external_reference'])){
+                //             $external_reference = $payment['external_reference'];
+                //         }else if(isset($payment->external_reference)){
+                //             $external_reference = $payment->external_reference;
+                //         }
+
+                //         if(isset($payment_status) && isset($external_reference)){
+            
+                //             if($payment_status == "cancelled" || $payment_status == "rejected" || $payment_status == "refunded" || $payment_status == "charged_back"){
+                //                 try {
+                //                     Mail::to("ecommerce@hieloyaventura.com")->send(new MercadoPagoNotification($payment_status, $id, $external_reference));                        
+                //                 } catch (Exception $e) {
+                //                     Log::channel("notificationmperror")->info("error: " . $e->getMessage() . ", error en envio de mail a ventas@hieloyaventura.com, numero de pago: $id" . ", line: " . $e->getLine());
+                //                 }
+                //             }
+
+                //         }else{
+                //             Log::channel("notificationmperror")->info("error: error en envio de mail a ventas@hieloyaventura.com, no se encontro payment status o external reference. Numero de pago: $id");
+                //         }
+        
+                //         Log::channel("notificationmp")->info("response payment json: " . json_encode($response_payment->json()));
+                //     }
+                // }else{
+                //     Log::channel("notificationmperror")->info('error: ID no encontrado');
+                // }
+            }
         } catch (Exception $e) {
             Log::channel("notificationmperror")->info('error: ' . $e->getMessage() . ', line: ' . $e->getLine());
         }
