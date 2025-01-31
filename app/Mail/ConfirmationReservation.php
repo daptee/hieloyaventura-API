e5c8185f2e39a02ed35239764bfb5a47dffb74c9
Sebastian Larramendy <info@daptee.com.ar>
2025-01-31 19:17:34 -0300

    vuelta atras, queda lo uktimo subido por enzo

--- app/Mail/ConfirmationReservation.php
+++ app/Mail/ConfirmationReservation.php
@@ -21,11 +21,11 @@ class ConfirmationReservation extends Mailable
      */
     public function __construct($data, $request)
     {
-        /* $this->data = $data;
-        $this->request = $request;
-        $this->turn = $request->turn ?? $data->turn->format('H:i\h\s');     
-        $this->subject = "Confirmacion reserva generada - Nro $data->reservation_number - Hielo & Aventura"; */
         $this->data = $data;
+        $this->request = $request;
+        $this->turn = $request->turn ?? $data->turn->format('H:i\h\s');
+        $this->subject = "Confirmacion reserva generada - Nro $data->reservation_number - Hielo & Aventura";
+        /* $this->data = $data;
         $this->request = $request;
         if ($data->is_transfer == "true") { // Si tiene traslado
             $this->turn = isset($data->hotel_id) && $data->hotel_id == 225
@@ -34,7 +34,7 @@ class ConfirmationReservation extends Mailable
         } else { // Sin traslado
             $this->turn = $data->turn->format('H:i\h\s');
         }
-        $this->subject = "Confirmacion reserva generada - Nro $data->reservation_number - Hielo & Aventura";
+        $this->subject = "Confirmacion reserva generada - Nro $data->reservation_number - Hielo & Aventura"; */
     }
 
     /**
