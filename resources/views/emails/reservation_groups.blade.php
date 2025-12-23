724ad68a176f04c898b6f15452007f1f2d1695f7
EnzoAmarilla <enzo100amarilla@gmail.com>
2025-12-23 12:00:12 -0300

    Testeamos no enviar archivos adjuntos en ep grupos

--- resources/views/emails/reservation_groups.blade.php
+++ resources/views/emails/reservation_groups.blade.php
@@ -6,25 +6,42 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Solicitud de grupos</title>
     <style>
-        body { font-family: Arial, Helvetica, sans-serif; color: #333; }
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+            color: #333;
+        }
     </style>
 </head>
 <body>
-    <p>
-        El Usuario {{ $mailData['nombreUsuario'] ?? 'N/D' }} de la agencia {{ $mailData['nombreAgencia'] ?? 'N/D' }} ha adjuntado archivos correspondiente a la solicitud de grupos nro. {{ $id_solicitud }}.
-    </p>
+
+    @if (!empty($files) && count($files) > 0)
+        <p>
+            El usuario <strong>{{ $mailData['nombreUsuario'] ?? 'N/D' }}</strong>
+            de la agencia <strong>{{ $mailData['nombreAgencia'] ?? 'N/D' }}</strong>
+            ha adjuntado archivos correspondientes a la solicitud de grupos
+            nro. <strong>{{ $id_solicitud }}</strong>.
+        </p>
+    @else
+        <p>
+            El usuario <strong>{{ $mailData['nombreUsuario'] ?? 'N/D' }}</strong>
+            de la agencia <strong>{{ $mailData['nombreAgencia'] ?? 'N/D' }}</strong>
+            ha enviado una solicitud de grupos
+            nro. <strong>{{ $id_solicitud }}</strong>.
+        </p>
+    @endif
 
     <p>
-        Los datos de la solicitud son:<br>
-        Excursion: {{ $mailData['nombreExcursion'] ?? 'N/D' }}<br>
-        Turno: {{ $mailData['turno'] ?? 'N/D' }}<br>
-        Nombre reserva: {{ $mailData['nombreReserva'] ?? 'N/D' }}<br>
-        Cantidad de pasajeros: {{ $mailData['cantPasajeros'] ?? 'N/D' }}
+        Los datos de la solicitud son:<br><br>
+
+        <strong>Excursión:</strong> {{ $mailData['nombreExcursion'] ?? 'N/D' }}<br>
+        <strong>Turno:</strong> {{ $mailData['turno'] ?? 'N/D' }}<br>
+        <strong>Nombre de la reserva:</strong> {{ $mailData['nombreReserva'] ?? 'N/D' }}<br>
+        <strong>Cantidad de pasajeros:</strong> {{ $mailData['cantPasajeros'] ?? 'N/D' }}
     </p>
 
     <p>
         Muchas gracias.<br>
-        El equipo de Hielo & Aventura
+        El equipo de <strong>Hielo &amp; Aventura</strong>
     </p>
 
 </body>
